0147762ac6cc0a0a5b6e08f96f146cd7fcb9c830
Arunava Das <64685513+arunava-webdev4u@users.noreply.github.com>
2023-08-19 12:32:16 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,ts}",
+      ],
+    theme: {
+        extend: {
+            colors: {
+                'background-light': '#ECF0F3',
+                'logo': '#Ec4d37'
+            }
+        },
+    },
+    plugins: [],
+}
+
